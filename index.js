8770f83fbd7f6360b464312f5c76acba9d541be0
Julius Tandan <julius.tandan@gmail.com>
2024-07-10 14:04:29 +0400

    updated

---
+++ index.js
@@ -0,0 +1,66 @@
+var tasks = [];
+var count = 0;
+
+var addBtn = document.getElementById("addTaskButton");
+addBtn.addEventListener("click", () => {
+  var userInput = document.getElementById("taskInput");
+  var userInputValue = userInput.value;
+  if (userInput.value != "") {
+    addTask(userInput.value);
+    userInput.value = "";
+  } else alert("Empty task ! ! ! ");
+});
+
+function addTask(task) {
+  count++;
+  tasks.push({ id: count, text: task });
+  showTasks();
+  console.log(tasks);
+}
+
+function showTasks() {
+  var taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+  document.body.appendChild(taskList);
+  tasks.forEach((item) => {
+    console.log(item, "single task");
+    var li = document.createElement("li");
+    var p = document.createElement("p");
+    li.classList = "list-group-item d-flex justify-content-between";
+    p.textContent = item.text;
+    li.appendChild(p);
+    taskList.appendChild(li);
+    var icons = document.createElement("div");
+    icons.classList = "icons";
+    li.appendChild(icons);
+    var trashIcon = document.createElement("img");
+    var editIcon = document.createElement("img");
+    trashIcon.src = "./trash.svg";
+    editIcon.src = "edit.svg";
+    icons.appendChild(trashIcon);
+    icons.appendChild(editIcon);
+
+    trashIcon.addEventListener("click", () => {
+      const trashModal = new bootstrap.Modal(
+        document.getElementById("exampleModal-delete")
+      );
+
+      trashModal.show();
+    });
+    editIcon.addEventListener("click", () => {
+      const editModal = new bootstrap.Modal(
+        document.getElementById("exampleModal")
+      );
+
+      editModal.show();
+    });
+  });
+}
+
+const deletebtn = document.getElementById("confirmDeleteButton");
+deletebtn.addEventListener("click", () => {
+  var trashModal = bootstrap.Modal.getInstance(
+    document.getElementById("exampleModal-delete")
+  );
+  trashModal.hide();
+});
